50bfd3d4de821d03f241cfad428fc788a157615d
Nur Asiah <nur@idtrust.id>
2024-03-25 23:13:46 +0700

    update animation demosection

--- src/components/Home/DisplayDemo.jsx
+++ src/components/Home/DisplayDemo.jsx
@@ -1,48 +1,24 @@
 "use client"
 
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { animationCardProfile } from "./animationCardProfile";
 
 export const HomeDisplayDemo = () => {
 
     const users = [
-        {"value":"client", "name":"Client"},
-        {"value":"sales", "name":"Sales"},
-        {"value":"sales-manager", "name":"Sales Manager"},
+        { "value": "client", "name": "Client" },
+        { "value": "sales", "name": "Sales" },
+        { "value": "sales-manager", "name": "Sales Manager" },
     ]
 
+    const [ready, setReady] = useState(true);
     const bannerRefs = useRef([]);
     const labelRefs = useRef([]);
 
     useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("animate"); // Add the CSS class to trigger animation
-                } else {
-                    entry.target.classList.remove("animate"); // Remove the CSS class if not in viewport
-                }
-            });
-        });
-
-        bannerRefs.current.forEach((ref) => {
-            observer.observe(ref);
-        });
-        labelRefs.current.forEach((ref) => {
-            observer.observe(ref);
-        });
-
-        return () => {
-            observer.disconnect();
-        };
-    }, []);
-
-    const handleAnimationEnd = (index) => {
-        const badgeProfile = bannerRefs.current[index].querySelector('.badge-profile');
-        if (badgeProfile) {
-            badgeProfile.classList.add("animate");
-        }
-    };
+        animationCardProfile(bannerRefs, labelRefs)
+    }, [ready]);
 
     return (
         <div className="w-full flex items-center justify-center relative">
@@ -54,17 +30,19 @@ export const HomeDisplayDemo = () => {
                     <Image src="/images/POS - Catalog - Tablet screen.jpg" width={1170} height={1170} alt="POS - Catalog" />
                 </div>
 
-                
+
                 {users.map((user, index) => (
-                    <div ref={(el) => (bannerRefs.current[index] = el)} 
-                    onAnimationEnd={() => handleAnimationEnd(index)}
-                    className={"profile-card card-"+user.value} 
-                    key={index} 
-                     style={{ animationDelay: `${index * 1}s` }}>
+                    <div ref={(el) => (bannerRefs.current[index] = el)}
+
+                        className={"profile-card card-" + user.value}
+                        key={index}
+                        style={{ animationDelay: `${index * 1}s` }}>
                         <div className="relative flex flex-col items-center justify-center" >
-                            <Image src={"/images/profile/"+user.value+".jpg"} width={127} height={127} alt={user.value} className="profile" />
-                            <span  className={"opacity-0 badge-profile "+user.value+" badge-1"}>
-                                        {user.name}
+                            <Image src={"/images/profile/" + user.value + ".jpg"} width={127} height={127} alt={user.value} className="profile" />
+                            <span ref={(el) => (labelRefs.current[index] = el)}
+                                style={{ animationDelay: `${(index*1) + 0.8}s` }}
+                                className={"opacity-0 badge-profile " + user.value + " badge-1"}>
+                                {user.name}
                             </span>
                         </div>
                     </div>
---
+++ src/components/Home/animationCardProfile.jsx
@@ -0,0 +1,30 @@
+export function animationCardProfile(bannerRefs, labelRefs) {
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('onAnimateProfile');
+            } else {
+                entry.target.classList.remove('onAnimateProfile');
+            }
+        });
+    }, options);
+
+    bannerRefs.current.forEach((ref) => {
+        observer.observe(ref);
+    });
+
+    labelRefs.current.forEach((ref) => {
+        observer.observe(ref);
+    });
+
+    return () => {
+        observer.disconnect();
+    };
+}
